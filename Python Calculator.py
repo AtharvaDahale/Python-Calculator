04d92aed3141f1326f649c18295bc1e788bb0ae5
AtharvaDahale <87626251+AtharvaDahale@users.noreply.github.com>
2021-08-27 19:45:45 +0530

    Add files via upload

---
+++ Python Calculator.py
@@ -0,0 +1,30 @@
+calculator = "Python Calculator"
+print("calculator")
+
+# Addition
+print("Addition-")
+first = input("  First: ")
+second = input("  Second: ")
+sum = float(first) + float(second)
+print("  Sum: " + str(sum))
+
+#Subtraction
+print("Subtraction-")
+first = input("  First: ")
+second = input("  Second: ")
+difference = float(first) - float(second)
+print("  Difference: " + str(difference))
+
+# Multiplication
+print("Multiplication-")
+first = input("  First: ")
+second = input("  Second: ")
+product = float(first) * float(second)
+print("  Product: " + str(product))
+
+# Division
+print("Division")
+first = input("  First: ")
+second = input("  Second: ")
+quotient = float(first) / float(second)
+print("  Quotient: " + str(quotient))
